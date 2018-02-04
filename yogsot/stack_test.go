bf36091981ad4d147e3f874825ad2ff1b9120e35
Gergely Brautigam <skarlso777@gmail.com>
2018-02-04 22:14:14 +0100

    Fixed unit test for stack.

--- yogsot/stack_test.go
+++ yogsot/stack_test.go
@@ -1,10 +1,59 @@
 package yogsot
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
+
+	"github.com/digitalocean/godo"
+	"golang.org/x/net/context"
 )
 
+var (
+	mux *http.ServeMux
+
+	ctx = context.TODO()
+
+	client *godo.Client
+
+	server *httptest.Server
+)
+
+func setup() {
+	mux = http.NewServeMux()
+	server = httptest.NewServer(mux)
+
+	client = godo.NewClient(nil)
+	url, _ := url.Parse(server.URL)
+	client.BaseURL = url
+}
+
+func teardown() {
+	server.Close()
+}
+
+func testMethod(t *testing.T, r *http.Request, expected string) {
+	if expected != r.Method {
+		t.Errorf("Request method = %v, expected %v", r.Method, expected)
+	}
+}
+
+func newTestClient() *YogClient {
+	yogClient := YogClient{client}
+	return &yogClient
+}
+
 func TestCreateStack(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/v2/droplets", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, http.MethodPost)
+		fmt.Fprint(w, `{"droplets": [{"id":1},{"id":2}]}`)
+	})
+
 	template := []byte(`
   Parameters:
     StackName:
@@ -23,7 +72,7 @@ func TestCreateStack(t *testing.T) {
       Image:
         Slug: "ubuntu-14-04-x64"`)
 	request := CreateStackRequest{TemplateBody: template, StackName: "TestStack"}
-	yogClient := NewClient("testToken")
+	yogClient := newTestClient()
 	response, err := yogClient.CreateStack(request)
 	if err != nil {
 		t.Fatal("unexpected error: " + err.Error())
