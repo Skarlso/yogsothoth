99926349719fb408dd56d28b27111e6e84b090f3
Gergely Brautigam <skarlso777@gmail.com>
2018-02-24 22:45:44 +0100

    Using wrapper.

--- yogsot/firewalls.go
+++ yogsot/firewalls.go
@@ -14,6 +14,14 @@ type Firewall struct {
 	OutboundDropletNames map[string][]string
 }
 
+// Rule is an in-outboundrule struct wrapper for ease of use
+type Rule struct {
+	*godo.InboundRule
+	*godo.OutboundRule
+	In           bool
+	DropletNames []string
+}
+
 func (fw *Firewall) buildRequest(stackname string, resource map[string]interface{}) error {
 	fw.DropletNames = make([]string, 0)
 	fw.InboundDropletNames = make(map[string][]string, 0)
@@ -23,24 +31,25 @@ func (fw *Firewall) buildRequest(stackname string, resource map[string]interface
 		if k == "Type" {
 			continue
 		}
-
 		switch k {
 		case "Name":
 			req.Name = v.(string)
 		case "InboundRules":
 			inboundRules := []godo.InboundRule{}
 			for inKey, value := range v.(map[interface{}]interface{}) {
-				iRule, dropletNames := getIRule(value.(map[interface{}]interface{}))
-				fw.InboundDropletNames[inKey.(string)] = dropletNames
-				inboundRules = append(inboundRules, iRule)
+				rule := new(Rule)
+				rule.generateInbound(value.(map[interface{}]interface{}))
+				fw.InboundDropletNames[inKey.(string)] = rule.DropletNames
+				inboundRules = append(inboundRules, *rule.InboundRule)
 			}
 			req.InboundRules = inboundRules
 		case "OutboundRules":
 			outboundRules := []godo.OutboundRule{}
 			for outKey, value := range v.(map[interface{}]interface{}) {
-				oRule, dropletNames := getORule(value.(map[interface{}]interface{}))
-				fw.OutboundDropletNames[outKey.(string)] = dropletNames
-				outboundRules = append(outboundRules, oRule)
+				rule := new(Rule)
+				rule.generateOutbound(value.(map[interface{}]interface{}))
+				fw.OutboundDropletNames[outKey.(string)] = rule.DropletNames
+				outboundRules = append(outboundRules, *rule.OutboundRule)
 			}
 			req.OutboundRules = outboundRules
 		case "Tags":
@@ -75,96 +84,108 @@ func (fw *Firewall) setFirewallDropletIDs(ids []int) {
 }
 
 func (fw *Firewall) setInboundDropletIDs(ids []int) {
+	// for _, in := range fw.Request.InboundRules {
+
+	// }
 }
 
 func (fw *Firewall) setOutboundDropletIDs(ids []int) {
 }
 
-func getIRule(v map[interface{}]interface{}) (iRule godo.InboundRule, dropletNames []string) {
+func (r *Rule) generateInbound(v map[interface{}]interface{}) {
+	r.InboundRule = new(godo.InboundRule)
 	for key, innerV := range v {
 		if key == "Protocol" {
-			iRule.Protocol = innerV.(string)
+			r.InboundRule.Protocol = innerV.(string)
 		}
 		if key == "PortRange" {
-			iRule.PortRange = innerV.(string)
+			r.InboundRule.PortRange = innerV.(string)
 		}
 		if key == "Sources" {
-			dropletNames, iRule.Sources = getSources(innerV.(map[interface{}]interface{}))
+			r.getSources(innerV.(map[interface{}]interface{}))
 		}
 	}
-	return
 }
 
-func getORule(v map[interface{}]interface{}) (oRule godo.OutboundRule, dropletNames []string) {
+func (r *Rule) generateOutbound(v map[interface{}]interface{}) {
+	r.OutboundRule = new(godo.OutboundRule)
 	for key, innerV := range v {
 		if key == "Protocol" {
-			oRule.Protocol = innerV.(string)
+			r.OutboundRule.Protocol = innerV.(string)
 		}
 		if key == "PortRange" {
-			oRule.PortRange = innerV.(string)
+			r.OutboundRule.PortRange = innerV.(string)
 		}
-		if key == "Sources" {
-			dropletNames, oRule.Destinations = getDestinations(innerV.(map[interface{}]interface{}))
+		if key == "Destinations" {
+			r.getDestinations(innerV.(map[interface{}]interface{}))
 		}
 	}
-	return
 }
 
-// TODO: Find a way to remove this duplication and tiet coupling to the
-// design of the sources struct.
-// Maybe submit a PR to remove the un-needed extra Destination struct.
-// Which is literally the same as Sources.
-func getSources(sources map[interface{}]interface{}) ([]string, *godo.Sources) {
-	ret := &godo.Sources{}
-	dropletNames, source := getSource(sources)
-	ret.Addresses = source["Addresses"].([]string)
-	ret.DropletIDs = source["DropletIDs"].([]int)
-	ret.LoadBalancerUIDs = source["LoadBalancerUIDs"].([]string)
-	ret.Tags = source["Tags"].([]string)
-	return dropletNames, ret
-}
-
-func getDestinations(destination map[interface{}]interface{}) ([]string, *godo.Destinations) {
-	ret := &godo.Destinations{}
-	dropletNames, source := getSource(destination)
-	ret.Addresses = source["Addresses"].([]string)
-	ret.DropletIDs = source["DropletIDs"].([]int)
-	ret.LoadBalancerUIDs = source["LoadBalancerUIDs"].([]string)
-	ret.Tags = source["Tags"].([]string)
-	return dropletNames, ret
-}
-
-func getSource(source map[interface{}]interface{}) ([]string, map[string]interface{}) {
-	retMap := make(map[string]interface{}, 0)
-	retMap["Addresses"] = make([]string, 0)
-	retMap["Tags"] = make([]string, 0)
-	retMap["DropletIDs"] = make([]int, 0)
-	retMap["LoadBalancerUIDs"] = make([]string, 0)
-	dropletNames := make([]string, 0)
-	for k, v := range source {
+func (r *Rule) getSources(sources map[interface{}]interface{}) {
+	r.DropletNames = make([]string, 0)
+	r.InboundRule.Sources = new(godo.Sources)
+	r.InboundRule.Sources.Addresses = make([]string, 0)
+	r.InboundRule.Sources.Tags = make([]string, 0)
+	r.InboundRule.Sources.DropletIDs = make([]int, 0)
+	r.InboundRule.Sources.LoadBalancerUIDs = make([]string, 0)
+	for k, v := range sources {
 		switch k {
 		case "Addresses":
 			for _, value := range v.([]interface{}) {
-				retMap["Addresses"] = append(retMap["Addresses"].([]string), value.(string))
+				r.InboundRule.Sources.Addresses = append(r.InboundRule.Sources.Addresses, value.(string))
 			}
 		case "Tags":
 			for _, value := range v.([]interface{}) {
-				retMap["Tags"] = append(retMap["Tags"].([]string), value.(string))
+				r.InboundRule.Sources.Tags = append(r.InboundRule.Sources.Tags, value.(string))
 			}
 		case "DropletIDs":
 			for _, value := range v.([]interface{}) {
 				switch o := value.(type) {
 				case string:
-					dropletNames = append(dropletNames, o)
+					r.DropletNames = append(r.DropletNames, o)
 				case int:
-					retMap["DropletIDs"] = append(retMap["DropletIDs"].([]int), o)
+					r.InboundRule.Sources.DropletIDs = append(r.InboundRule.Sources.DropletIDs, o)
 				}
 			}
 		case "LoadBalancerUIDs":
 			for _, value := range v.([]interface{}) {
-				retMap["LoadBalancerUIDs"] = append(retMap["LoadBalancerUIDs"].([]string), value.(string))
+				r.InboundRule.Sources.LoadBalancerUIDs = append(r.InboundRule.Sources.LoadBalancerUIDs, value.(string))
 			}
 		}
 	}
-	return dropletNames, retMap
+}
+
+func (r *Rule) getDestinations(destinations map[interface{}]interface{}) {
+	r.DropletNames = make([]string, 0)
+	r.OutboundRule.Destinations = new(godo.Destinations)
+	r.OutboundRule.Destinations.Addresses = make([]string, 0)
+	r.OutboundRule.Destinations.Tags = make([]string, 0)
+	r.OutboundRule.Destinations.DropletIDs = make([]int, 0)
+	r.OutboundRule.Destinations.LoadBalancerUIDs = make([]string, 0)
+	for k, v := range destinations {
+		switch k {
+		case "Addresses":
+			for _, value := range v.([]interface{}) {
+				r.OutboundRule.Destinations.Addresses = append(r.OutboundRule.Destinations.Addresses, value.(string))
+			}
+		case "Tags":
+			for _, value := range v.([]interface{}) {
+				r.OutboundRule.Destinations.Tags = append(r.OutboundRule.Destinations.Tags, value.(string))
+			}
+		case "DropletIDs":
+			for _, value := range v.([]interface{}) {
+				switch o := value.(type) {
+				case string:
+					r.DropletNames = append(r.DropletNames, o)
+				case int:
+					r.OutboundRule.Destinations.DropletIDs = append(r.OutboundRule.Destinations.DropletIDs, o)
+				}
+			}
+		case "LoadBalancerUIDs":
+			for _, value := range v.([]interface{}) {
+				r.OutboundRule.Destinations.LoadBalancerUIDs = append(r.OutboundRule.Destinations.LoadBalancerUIDs, value.(string))
+			}
+		}
+	}
 }
